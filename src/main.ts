923099fa03d6e5f9636a4be3787bebd199a68809
Illyrix <Illyrix@outlook.com>
2018-06-24 18:21:09 +0800

    🎨 add: spirit, control & render path

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,5 @@ import * as MapGenerator from 'vaoc-map-generator'
 console.log(MapGenerator)
 
 let app = new PIXI.Application(800, 600, {backgroundColor: 0x333})
-const appContainer: any = document.getElementById('app')
+const appContainer: HTMLDivElement = document.getElementById('app') as HTMLDivElement
 appContainer.appendChild(app.view)
