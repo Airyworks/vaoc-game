528a923fc0f8734a738ba2895c9eb15db7b23eb0
Matt <you1474600@gmail.com>
2018-06-25 17:05:08 +0800

    ➖ remove jest test

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -71,8 +71,7 @@ module.exports = {
     ]
   },
   node: {
-    // prevent webpack from injecting useless setImmediate polyfill because Vue
-    // source contains it (although only uses it if it's native).
+    // prevent webpack from injecting useless setImmediate polyfill
     setImmediate: false,
     // prevent webpack from injecting mocks to Node native modules
     // that does not make sense for the client
