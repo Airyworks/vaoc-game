08af96ab8fc767b5345bae211481310dfafcf0cd
Illyrix <Illyrix@outlook.com>
2018-07-22 04:11:00 +0800

    🔧 chore: character ui

--- src/ui/battleground/character.ts
+++ src/ui/battleground/character.ts
@@ -1,3 +1,10 @@
-export class Character {
-  
+import * as PIXI from 'pixi.js'
+import { Character as Char } from '../../component/battle/character'
+
+export class Character extends PIXI.Container {
+  public info: Char
+  constructor(info: Char) {
+    super()
+    this.info = info
+  }
 }
