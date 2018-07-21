563f04fb20e612e3787f7aafc9298d0b7eabe208
Matt <you1474600@gmail.com>
2018-07-22 00:47:13 +0800

    ✨ card collection

--- src/ui/collection/container.ts
+++ src/ui/collection/container.ts
@@ -1,15 +1,59 @@
 import * as PIXI from 'pixi.js'
 import loader from '../../util/loader'
+import { CARD_TITLE_WORD_STYLE, CARD_ATTR_WORD_STYLE } from '../../config'
 
 interface ITexture {
   [propName: string]: PIXI.Texture
 }
 
+interface IPrefer {
+  [propName: string]: string
+}
+
+interface ICard {
+  name: string,
+  hp: number,
+  magic: number,
+  speed: number
+}
+
+const cardTitleStyle = new PIXI.TextStyle(CARD_TITLE_WORD_STYLE)
+const cardAttrStyle = new PIXI.TextStyle(CARD_ATTR_WORD_STYLE)
+
 export class Collection extends PIXI.Container {
   private store: ITexture = {}
-  private cards = [{  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }]
+  private cards: ICard[] = [
+    { name: 'WJX', hp: 10, magic: 15, speed: 11 },
+    { name: 'WJX', hp: 15, magic: 10, speed: 12 },
+    { name: 'WJX', hp: 12, magic: 15, speed: 13 },
+    { name: 'WJX', hp: 14, magic: 25, speed: 15 },
+    { name: 'WJX', hp: 10, magic: 11, speed: 16 },
+    { name: 'WJX', hp: 10, magic: 15, speed: 15 },
+    { name: 'WJX', hp: 10, magic: 15, speed: 15 },
+    { name: 'WJX', hp: 10, magic: 15, speed: 15 },
+    { name: 'Minako', hp: 10, magic: 15, speed: 15 },
+    { name: 'Minako', hp: 10, magic: 15, speed: 15 },
+    { name: 'Minako', hp: 10, magic: 15, speed: 15 },
+    { name: 'Minako', hp: 10, magic: 15, speed: 15 },
+    { name: 'Minako', hp: 10, magic: 15, speed: 15 },
+    { name: 'Minako', hp: 10, magic: 15, speed: 15 },
+    { name: 'Minako', hp: 10, magic: 15, speed: 15 }
+  ]
   private index = 0
   private cardContainer = new PIXI.Container()
+  private prefer: IPrefer = {
+    wid: 'collection-card-wid.png',
+    wat: 'collection-card-wat.png',
+    tim: 'collection-card-tim.png',
+    spa: 'collection-card-spa.png',
+    soi: 'collection-card-soi.png',
+    none: 'collection-card-none.png',
+    lig: 'collection-card-lig.png',
+    fir: 'collection-card-fir.png',
+    ele: 'collection-card-ele.png',
+    dar: 'collection-card-dar.png'
+  }
+  private preferIndexes = Object.keys(this.prefer)
 
   constructor() {
     super()
@@ -108,7 +152,8 @@ export class Collection extends PIXI.Container {
   }
 
   protected _drawCards() {
-    const card = this.store['collection-card.png']
+    const card = this.store['collection-card-front-1.png']
+    const cardIcon = this.store['collection-icon.png']
     const shadow = this.store['collection-card-shadow.png']
     const baseX = 70
     const baseY = 77
@@ -117,13 +162,49 @@ export class Collection extends PIXI.Container {
       const x = (i % 4) * 168 + baseX
       const y = Math.floor(i / 4) * 235 + baseY
       const cardS = new PIXI.Sprite(card)
+      const cardIconS = new PIXI.Sprite(cardIcon)
       const shadowS = new PIXI.Sprite(shadow)
+      const preferIndex: string = this.preferIndexes[Math.floor(Math.random() * this.preferIndexes.length)]
+      const preferS = new PIXI.Sprite(this.store[this.prefer[preferIndex]])
+      const title = new PIXI.Text(this.cards[cardIndex].name, cardTitleStyle)
+      const hp = new PIXI.Text(this.cards[cardIndex].hp.toString(), cardAttrStyle)
+      const magic = new PIXI.Text(this.cards[cardIndex].magic.toString(), cardAttrStyle)
+      const speed = new PIXI.Text(this.cards[cardIndex].speed.toString(), cardAttrStyle)
       cardS.x = x
       cardS.y = y
       shadowS.x = x - 6
       shadowS.y = y - 6
+      preferS.anchor.set(0.5)
+      preferS.x = x + 120
+      preferS.y = y + 130
+      cardIconS.x = x + 60
+      cardIconS.y = y + 140
+      title.anchor.set(0.5)
+      title.x = x + 73
+      title.y = y + 15
+      hp.x = x + 35
+      magic.x = x + 35
+      speed.x = x + 35
+      hp.y = y + 140
+      magic.y = y + 155
+      speed.y = y + 170
+
+      const cardDemo = new PIXI.Sprite(this.store['collection-card-demo.png'])
+      cardDemo.anchor.set(0.5)
+      cardDemo.x = x + 73
+      cardDemo.y = y + 80
+      // cardDemo.scale.x = 1.32
+      // cardDemo.scale.y = 1.42
+
       this.cardContainer.addChild(shadowS)
+      this.cardContainer.addChild(cardDemo)
       this.cardContainer.addChild(cardS)
+      this.cardContainer.addChild(preferS)
+      this.cardContainer.addChild(cardIconS)
+      this.cardContainer.addChild(title)
+      this.cardContainer.addChild(hp)
+      this.cardContainer.addChild(magic)
+      this.cardContainer.addChild(speed)
     }
   }
 }
